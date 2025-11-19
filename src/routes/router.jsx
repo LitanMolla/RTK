bba97c36b7e9f4f64082814a532a1fd82115f046
LitanMolla <litanmolla9@gmail.com>
2025-11-20 02:51:23 +0600

    NotFound added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router'
 import Root from '../layouts/Root'
 import Home from '../pages/Home/Home'
+import NotFound from '../pages/NotFound/NotFound'
 
 const router = createBrowserRouter([
     {
         path: '/',
         Component: Root,
+        errorElement: <NotFound/>,
         children: [
             {
                 index: true,
